af87c0523b34adede126035ba59abaeab4250591
unknown <157027148@qq.com>
2020-11-07 16:07:57 +0800

    init

---
+++ src/main/java/csmen/group/project/Controller/myController.java
@@ -0,0 +1,14 @@
+package csmen.group.project.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class myController {
+
+    @RequestMapping("")
+    public String HomePage() {
+        return "Home";
+    }
+}
