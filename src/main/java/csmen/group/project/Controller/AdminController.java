73b845386b3c0dab65ed8d98bd34581642e511f8
jola <jola@covariant.cn>
2020-11-13 19:19:50 +0800

    admin func

--- src/main/java/csmen/group/project/Controller/AdminController.java
+++ src/main/java/csmen/group/project/Controller/AdminController.java
@@ -9,10 +9,13 @@ import csmen.group.project.entity.DoctorInfo;
 import csmen.group.project.entity.UserInfo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.print.Doc;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -40,13 +43,13 @@ public class AdminController {
         AdminInfo aa = ad.login(admin);
         if(aa == null){
             model.addAttribute("msg","admin login error");
-            return "public/False";
+            return "Public/Fail";
         }
         Integer HID = aa.getHID();
         String privilege = aa.getPrivilege();
         HttpSession session = request.getSession();
-        session.setAttribute("aname",aa.getName());
-        session.setAttribute("apassword",aa.getPassword());
+        session.setAttribute("aname",admin.getName());
+        session.setAttribute("apassword",admin.getPassword());
         model.addAttribute("admin", aa);
         List<UserInfo> userList = null;
         List<DoctorInfo> doctorList = null;
@@ -68,5 +71,84 @@ public class AdminController {
             return "Public/False";
         }
         return "AdminControll/ControllIndex";
+    }
+
+    @RequestMapping("/adminindex")
+    public String AdminIndex(AdminInfo admin, Model model, HttpServletRequest request){
+        HttpSession session = request.getSession();
+        admin.setName((String)session.getAttribute("aname"));
+        admin.setPassword((String)session.getAttribute("apassword"));
+        AdminInfo aa = ad.login(admin);
+        if(aa == null){
+            model.addAttribute("msg","admin login error");
+            return "Public/Fail";
+        }
+        Integer HID = aa.getHID();
+        String privilege = aa.getPrivilege();
+        model.addAttribute("admin", aa);
+        List<UserInfo> userList = null;
+        List<DoctorInfo> doctorList = null;
+        List<AdminInfo> adminList = null;
+        if(privilege.equals("Hyper")) {
+            userList = ud.findAll();
+            doctorList = dd.findAll();
+            adminList = ad.findAll();
+            model.addAttribute("ulist", userList);
+            model.addAttribute("dlist",doctorList);
+            model.addAttribute("alist",adminList);
+        }else if(privilege.equals("Hospital")){
+            doctorList = dd.findByHID(HID);
+            model.addAttribute("dlist",doctorList);
+        }else if(privilege.equals("Commulity")){
+            userList = ud.findAll();
+            model.addAttribute("ulist",userList);
+        }else{
+            return "Public/Fail";
+        }
+        return "AdminControll/ControllIndex";
+    }
+
+    @RequestMapping("/goupdatedoctor/{DID}")
+    public String GoUpdateDoctor(@PathVariable("DID") Integer did, Model model){
+        DoctorInfo doctor = dd.findByDID(did);
+        model.addAttribute("doctor",doctor);
+        return "AdminControll/Update/UpdateDoctor";
+    }
+
+    @RequestMapping("/updatedoctor")
+    @ResponseBody
+    public boolean updatedoctor(DoctorInfo doctor){
+        int i = dd.updateDoctor(doctor);
+        if(i > 0)
+            return true;
+        else
+            return false;
+    }
+
+    @RequestMapping("/godeldoctor/{DID}")
+    public String GoDelDoctor(@PathVariable("DID") Integer did, Model model){
+        model.addAttribute("href","/adminindex");
+        dd.delDoctor(did);
+        return "Public/Success";
+    }
+
+    @RequestMapping("/goadddoctor")
+    public String GoAddDoctor(AdminInfo admin, Model model, HttpServletRequest request){
+        HttpSession session = request.getSession();
+        admin.setName((String)session.getAttribute("aname"));
+        admin.setPassword((String)session.getAttribute("apassword"));
+        AdminInfo aa = ad.login(admin);
+        model.addAttribute("admin",aa);
+        return "AdminControll/Add/AddDoctor";
+    }
+
+    @RequestMapping("/adddoctor")
+    @ResponseBody
+    public boolean addDoctor(DoctorInfo doctor){
+        int i = dd.addDoctor(doctor);
+        if(i > 0)
+            return true;
+        else
+            return false;
     }
 }
--- src/main/java/csmen/group/project/dao/DoctorDao.java
+++ src/main/java/csmen/group/project/dao/DoctorDao.java
@@ -2,13 +2,22 @@ package csmen.group.project.dao;
 
 import csmen.group.project.entity.DoctorInfo;
 
+import javax.print.Doc;
 import java.util.List;
 
 public interface DoctorDao {
 
-    DoctorInfo login(DoctorInfo docter);
+    DoctorInfo login(DoctorInfo doctor);
 
     List<DoctorInfo> findAll();
 
     List<DoctorInfo> findByHID(Integer HID);
+
+    DoctorInfo findByDID(Integer DID);
+
+    int updateDoctor(DoctorInfo doctor);
+
+    int delDoctor(Integer DID);
+
+    int addDoctor(DoctorInfo doctor);
 }
