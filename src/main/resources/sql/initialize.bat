ae178767effe5d968886297357ea98e42c174690
KKKKri <74279375+KKKKri@users.noreply.github.com>
2020-12-19 14:38:39 +0800

    Update initialize.bat

--- src/main/resources/sql/initialize.bat
+++ src/main/resources/sql/initialize.bat
@@ -1,1 +1,11 @@
-
+CREATE DATABASE IF NOT EXISTS runoob;
+use runoob;
+set names utf8;
+source /sql/create_db.sql;
+source /sql/article.sql;
+source /sql/disease.sql;
+source /sql/doctor.sql;
+source /sql/forum.sql;
+source /sql/hostipal.sql;
+source /sql/user.sql;
+source /sql/reply.sql;
